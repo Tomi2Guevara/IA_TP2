af9fd44a693acd6f3dc4f7b2d43c38a1db71b8ca
Tomas Guevara <tomas.guevara@uncuyo.edu.ar>
2024-05-02 02:05:13 -0300

    versión 3.0

--- fuzzy.py
+++ fuzzy.py
@@ -1,27 +1,51 @@
 from antecedente import Antecedent
 class Fuzzy():
-    def __init__(self, pAnt = [], pCon = []):
+    def __init__(self, pAnt = [], pCon = [], pHs = [], pCon2 = []):
         self.ant = pAnt
         self.con = pCon
+        self.pHs = pHs
+        self.pCon2 = pCon2
         self.ez = Antecedent("eZ")
         self.dz = Antecedent("dZ")
 
-    def solution(self, tExt, tInt, tObj):
+    def solution(self, tExt, tInt, hr, pron):
+        vh = []
+        for i in self.pHs:
+            vh.append(i.eval(hr))
+
+        if vh[1] > vh[0] and vh[1] > vh[2]:
+            tObj = 25*vh[1]
+
+        elif pron > 25:
+            val = max(vh[0], vh[2])
+            tObj = 25 - 20 * vh[0]
+        else:
+            val = max(vh[0], vh[2])
+            tObj = 45 - 20 * val
+
         ez1 = tInt - tObj
         dz1 = tExt - tInt
         ve = []
         vd = []
+
+
         for a in self.ant:
             ve.append(a.eval(ez1))
             vd.append(a.eval(dz1))
         self.ez.val = ve
         self.dz.val = vd
+
+
+
+
         c = max(min(self.ez.val[0], self.dz.val[0]), min(self.ez.val[0], self.dz.val[1]), min(self.ez.val[1], self.dz.val[0]), min(self.ez.val[1], self.dz.val[2]), min(self.ez.val[2], self.dz.val[1]), min(self.ez.val[2], self.dz.val[2]))
         a = max(min(self.ez.val[0], self.dz.val[2]), min(self.ez.val[2], self.dz.val[0]))
         m = min(self.ez.val[1], self.dz.val[1])
+
         sc = self.con[0].bisect_defuzzify(c)
         sm = self.con[1].bisect_defuzzify(m)
         sa = self.con[2].bisect_defuzzify(a)
+
         if (a+m+c) == 0:
             aper = 0
         else:
--- main.py
+++ main.py
@@ -7,67 +7,53 @@ import csv
 import pandas as pd
 #Cargar datos
 
-# days = []
-# with open('temps.json') as file:
-#     data = json.load(file)
-#     for i in data:
-#         days.append(np.array(i))
+days = []
+with open('temps.json') as file:
+    data = json.load(file)
+    for i in data:
+        days.append(np.array(i))
 
-days = [[10, 9, 9, 9, 10, 10, 11, 11, 27, 28, 28, 29, 29, 30, 31, 31, 30, 30, 30, 29, 29, 28, 28, 27],
-        [10, 9, 9, 9, 10, 10, 11, 11, 27, 28, 28, 29, 29, 30, 31, 31, 30, 30, 30, 29, 29, 28, 28, 27]]
-days = [np.array(i) for i in days]
+# days = [[10, 9, 9, 9, 10, 10, 11, 11, 27, 28, 28, 29, 29, 30, 31, 31, 30, 30, 30, 29, 29, 28, 28, 27],
+#         [10, 9, 9, 9, 10, 10, 11, 11, 27, 28, 28, 29, 29, 30, 31, 31, 30, 30, 30, 29, 29, 28, 28, 27]]
+# days = [np.array(i) for i in days]
 
 
 #curvas de pertenencia
-#variables de entrada
+
 neg = Perten("n", True, -35, 0,[-35, 35], 0, 0.90)
 zero = Perten("z", False, -1, 2, [-35, 35], 0.5)
 pos = Perten("p", True, 0, 35, [-35, 35], 0.10, 1)
+#------------------
 cl = Perten("c", True, 0, 50, [0, 100], 0, 0.8)
 mid = Perten("m", False, 48, 4, [0, 100], 0.5)
 op = Perten("a", True, 50, 100, [0, 100], 0.2, 1)
-
-
-#d1 = np.random.uniform(10, 30, 24)
-#d2 = np.random.uniform(10, 30, 24)
-#d3 = np.random.uniform(10, 30, 24)
-#d4 = np.random.uniform(10, 30, 24)
-#temp_p_Hora = {}
+#------------------
+ad = Perten("ad", True, 0, 9, [0, 24], 0, 0.78)
+md = Perten("md", True, 7, 21, [0, 24], 0.1, 0.9)
+ng = Perten("ng", True, 19, 24, [0, 24], 0.22, 1)
+#------------------
+tmin = Perten("c", True, 5, 25, [0, 35], 0, 0.8)
+t= Perten("m", False, 22, 4, [0, 35], 0.5)
+tmax = Perten("a", True, 25, 35, [0, 35], 0.2, 1)
 
 tInt = 27
 tInts = [27]
 Apers = []
 tDays = []
 
-ctrol = Fuzzy([neg, zero, pos], [cl, mid, op])
+ctrol = Fuzzy([neg, zero, pos], [cl, mid, op], [ad, md, ng], [tmin, t, tmax])
 
 for j in range(len(days)-1):
 
     for i in range(len(days[j])):
-        #Subconjuntos:
 
-        if 7 < i:
-            if i < 20:
-                tInt, pAper = ctrol.solution(days[j][i], tInt, 25)
-                tDia = tInt
-                tDays.append(tDia)
-                #print(tInt)
-            elif days[j + 1].mean() > 25:
-                tInt, pAper = ctrol.solution(days[j][i], tInt, 5)
+        tInt, pAper = ctrol.solution(days[j][i], tInt,i,days[j + 1].mean())
 
-            elif days[j + 1].mean() < 20:
-                tInt, pAper = ctrol.solution(days[j][i], tInt, 40)
-
-        elif days[j].mean() > 25:
-            tInt, pAper = ctrol.solution(days[j][i], tInt, 5)
-
-        elif days[j].mean() < 25:
-            tInt, pAper = ctrol.solution(days[j][i], tInt, 40)
 
         tInts.append(tInt)
         Apers.append(pAper)
 
-    print('Temperatura del día:            '+ str(tDia))
+   #print('Temperatura del día:            '+ str(tDia))
     print('Temperatura promedio del día:   ' + str(days[j].mean()))
     print('' +
           '---------------------------------')
@@ -77,7 +63,7 @@ unir = []
 count = 0
 for i in days:
     count += 1
-    if count == 2:
+    if count == 3:
         break
     for j in i:
         unir.append(j)
@@ -85,18 +71,23 @@ for i in days:
 
 #graficar en el mismo grapico Apers y tInts
 # Add a title
-plt.title('Temperatura y Apertura por tiempo')
+plt.title('Fusificador')
 
 # Add labels for the x and y axes
 plt.xlabel('Tiempo (Hs)')
-plt.ylabel('Temperatura (°C)')
-plt.plot(tInts, label='Temperatura Interna')
-plt.legend()
-#plt.show()
-plt.plot(unir, label='Temperatura externa', color='red')
-plt.legend()
-plt.plot(Apers, label='Apertura', color='orange')
-plt.legend()
+plt.ylabel('(u)')
+# plt.plot(tInts, label='Temperatura Interna')
+# plt.legend()
+# #plt.show()
+# plt.plot(unir, label='Temperatura externa', color='red')
+# plt.legend()
+# plt.plot(Apers, label='Apertura', color='orange')
+# plt.legend()
+# plt.show()
+
+ad.plot()
+md.plot()
+ng.plot()
 plt.show()
 
 
